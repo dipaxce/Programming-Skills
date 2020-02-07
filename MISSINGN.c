4f0ad1ebfc98da0856223b2d0df6dd9ee107d383
dipaxce <dipaxce@gmail.com>
2020-02-08 00:38:24 +0545

    Err: Fixed the issue where the output was not being displayed

--- MISSINGN.c
+++ MISSINGN.c
@@ -3,13 +3,17 @@
 
 void main()
 {
+	int i, n, sum, a[] = {2,3,4,5};
 	clrscr();
-	int i, n= 5, sum, a[] = {1, 2, 3, 5};
+	printf("Enter the size of array:");
+	scanf("%d", &n);
+
+	printf("Enter the values in array:");
 	for (i=0; i<n; i++)
-     /*  {
+	{
 		scanf("%d", a[i]);
 	}
-     */
+
 	sum = (n+1)*(n+2)/2;
 
 	for (i =0; i<n; i++)
